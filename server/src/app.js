7ace5e3ec6d952c2d74813ef02d09d9d72dd3e64
todun <todun@users.noreply.github.com>
2019-02-19 11:32:02 +0000

    added mongoose to dependency to server for CRUD functionality; added mongoose connection to mongo database; added POST request and GET response; added post model to descibe the database schema

--- server/src/app.js
+++ server/src/app.js
@@ -8,13 +8,54 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+// app.get('/posts', (req, res) => {
+//   res.send(
+//     [{
+//       title: 'Hello World!',
+//       description: 'Hi there! How are you?'
+//     }]
+//   )
+// })
+
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/posts')
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error'))
+db.once('open', function (callback) {
+  console.log('Connection Succeeded')
+})
+
+var Post = require('../models/post')
+
+// Add new post
+app.post('/posts', (req, res) => {
+  // var db = req.db
+  var title = req.body.title
+  var description = req.body.description
+  var newPost = new Post({
+    title: title,
+    description: description
+  })
+
+  newPost.save(function (error) {
+    if (error) {
+      console.log(error)
+    }
+    res.send({
+      success: true,
+      message: 'Post saved successfully!'
+    })
+  })
+})
+
+// Fetch all posts
 app.get('/posts', (req, res) => {
-  res.send(
-    [{
-      title: 'Hello World!',
-      description: 'Hi there! How are you?'
-    }]
-  )
+  Post.find({}, 'title description', function (error, posts) {
+    if (error) { console.error(error) }
+    res.send({
+      posts: posts
+    })
+  }).sort({_id: -1})
 })
 
 app.listen(process.env.PORT || 8081)
